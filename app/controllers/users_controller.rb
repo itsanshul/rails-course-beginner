07111ef1623dab59956884afb209ca7fc3108ca7
Anshul <anshulkr1597@gmail.com>
2019-05-06 13:00:00 +0530

    Complete Admin Functionalities

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
 
   before_action :set_user, only: [:edit, :update, :show]
   before_action :require_same_user, only: [:edit, :update]
+  before_action :require_admin , only: [:destroy]
 
   def index
     @users = User.paginate(page: params[:page], per_page: 5)
@@ -14,6 +15,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       flash[:success] = "Welcome to Alpha Blog #{@user.username}"
       redirect_to articles_path
     else
@@ -35,6 +37,13 @@ class UsersController < ApplicationController
     @user_articles = @user.articles.paginate(page: params[:page], per_page: 2)
   end
 
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    flash[:danger] = "User and all Article created by user has been deleted"
+    redirect_to users_path
+  end
+
   private
   def user_params
     params.require(:user).permit(:username, :email, :password)
@@ -43,8 +52,15 @@ class UsersController < ApplicationController
     @user =  User.find(params[:id])
   end
   def require_same_user
-    if current_user != @user
+    if current_user != @user and !current_user.admin?
       flash[:danger] ="You can only edit your own account"
+      redirect_to root_path
+    end
+  end
+
+  def require_admin
+    if logged_in? and !current_user.admin?
+      flash[:danger] = "Only user can perform this action"
       redirect_to root_path
     end
   end
